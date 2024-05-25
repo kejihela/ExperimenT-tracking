26ce1a11dcb72a8d06f17658b04952a8b6b37cfe
Adebayo Abdulganiyu Keji <Gleebayour@gmail.com>
2024-05-25 22:29:02 +0000

    second_commit

--- train.py
+++ train.py
@@ -21,15 +21,23 @@ def load_pickle(filename: str):
     help="Location where the processed NYC taxi trip data was saved"
 )
 def run_train(data_path: str):
-
+    mlflow.sklearn.autolog()
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+    with mlflow.start_run():
+        mlflow.set_tag("developer", "adebayo")
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+        mlflow.log_param("train-data-path", "./data/green_tripdata_2023-01.parquet")
+        mlflow.log_param("valid-data-path", "./data/green_tripdata_2023-02.parquet")
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+
+        mlflow.log_metric("rmse", rmse)
+
+        #mlflow.log_artifact(local_path="models/lin_reg.bin", artifact_path="models_pickle")
 
 
 if __name__ == '__main__':
